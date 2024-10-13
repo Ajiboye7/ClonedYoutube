95a18078545296632bc645fef7a5ddf65d317552
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-13 21:03:28 +0100

    update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,14 @@ import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
-const Searc
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
       navigate(`/search/${searchTerm}`);
 
       setSearchTerm('');
