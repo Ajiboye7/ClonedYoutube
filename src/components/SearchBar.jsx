e83b9f49082d667c16d24d5072691b852187b905
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-16 19:49:03 +0100

    Input field fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -18,7 +18,17 @@ const SearchBar = () => {
   };
 
   return (
-    
+    <Paper
+      component='form'
+      onSubmit={onhandleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
       <input
         className='search-bar'
         placeholder='Search...'
