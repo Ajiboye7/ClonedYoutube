93929c4f2291a289e5e38ec6382f9396e21976c2
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-11-07 21:52:02 +0100

    upate

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -6,13 +6,7 @@ const options = {
   params: {
     maxResults: 50,
   },
-  headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-  },
-};
-
-export const fetchFromAPI = async (url) => {
+  headers:rl) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
   return data;
