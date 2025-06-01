5954d70798ff4f9ddd3dfa22c2ca19f1fbfd51a2
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-06-01 14:21:19 +0100

    updated

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -12,16 +12,6 @@ const ChannelDetail = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    const fetchResults = async () => {
-      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
-
-      setChannelDetail(data?.items[0]);
-
-      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
-
-      setVideos(videosData?.items);
-    };
-
     fetchResults();
   }, [id]);
 
