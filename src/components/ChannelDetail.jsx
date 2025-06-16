5c624a1fd175d9db507c60221ee99666445d9039
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-06-16 19:35:21 +0100

    updated

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -12,22 +12,7 @@ const ChannelDetail = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    const fetchResults = async () => {
-      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
-
-      setChannelDetail(data?.items[0]);
-
-      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
-
-      setVideos(videosData?.items);
-    };
-
-    fetchResults();
-  }, [id]);
-
-  return (
-    <Box minHeight="95vh">
-      <Box>
+    const fe
         <div style={{
           height:'300px',
           background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
