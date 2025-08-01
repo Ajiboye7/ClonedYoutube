78eeb5620898121d98b89ec8d6fbaa4dc351e1ce
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-08-01 10:20:56 +0100

    title change

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -16,7 +16,13 @@ const ChannelDetail = () => {
       const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
 
       setChannelDetail(data?.items[0]);
- 
+
+      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
   }, [id]);
 
   return (
